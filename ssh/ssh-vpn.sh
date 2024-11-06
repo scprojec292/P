adca1b850a76c2d5f48e69793dc6b21b933c9203
Andyyuda <128485068+Andyyuda@users.noreply.github.com>
2024-11-06 23:51:33 +0700

    Create ssh-vpn.sh

---
+++ ssh/ssh-vpn.sh
@@ -0,0 +1,424 @@
+#!/bin/bash
+rm -f $0
+apt dist-upgrade -y
+apt install netfilter-persistent -y
+apt-get remove --purge ufw firewalld -y
+apt install -y screen curl jq bzip2 gzip vnstat coreutils rsyslog iftop zip unzip git apt-transport-https build-essential -y
+REPO="https://raw.githubusercontent.com/kipasu/f/main/"
+# initializing var
+export DEBIAN_FRONTEND=noninteractive
+MYIP=$(wget -qO- ipinfo.io/ip)
+MYIP2="s/xxxxxxxxx/$MYIP/g"
+NET=$(ip -o $ANU -4 route show to default | awk '{print $5}')
+if [[ -f /etc/os-release ]]; then
+    . /etc/os-release
+    OS_NAME=$ID
+    OS_VERSION=$VERSION_ID
+
+    echo "Menemukan sistem operasi: $OS_NAME $OS_VERSION"
+else
+    echo "Tidak dapat menentukan sistem operasi."
+    exit 1
+fi
+
+#detail nama perusahaan
+country=ID
+state=Indonesia
+locality=Jakarta
+organization=none
+organizationalunit=none
+commonname=none
+email=none
+
+# simple password minimal
+curl -sS ${REPO}ssh/password | openssl aes-256-cbc -d -a -pass pass:scvps07gg -pbkdf2 > /etc/pam.d/common-password
+chmod +x /etc/pam.d/common-password
+
+# go to root
+cd
+
+# Edit file /etc/systemd/system/rc-local.service
+cat > /etc/systemd/system/rc-local.service <<-END
+[Unit]
+Description=/etc/rc.local
+ConditionPathExists=/etc/rc.local
+[Service]
+Type=forking
+ExecStart=/etc/rc.local start
+TimeoutSec=0
+StandardOutput=tty
+RemainAfterExit=yes
+SysVStartPriority=99
+[Install]
+WantedBy=multi-user.target
+END
+
+# nano /etc/rc.local
+cat > /etc/rc.local <<-END
+#!/bin/sh -e
+# rc.local
+# By default this script does nothing.
+exit 0
+END
+
+# Ubah izin akses
+chmod +x /etc/rc.local
+
+# enable rc local
+systemctl enable rc-local
+systemctl start rc-local.service
+
+# disable ipv6
+echo 1 > /proc/sys/net/ipv6/conf/all/disable_ipv6
+sed -i '$ i\echo 1 > /proc/sys/net/ipv6/conf/all/disable_ipv6' /etc/rc.local
+
+#update
+apt update -y
+apt upgrade -y
+apt dist-upgrade -y
+apt-get remove --purge ufw firewalld -y
+apt-get remove --purge exim4 -y
+
+#install jq
+apt -y install jq
+
+#install shc
+apt -y install shc
+
+# install wget and curl
+apt -y install wget curl
+
+#figlet
+apt-get install figlet -y
+apt-get install ruby -y
+gem install lolcat
+
+# set time GMT +7
+ln -fs /usr/share/zoneinfo/Asia/Jakarta /etc/localtime
+
+# set locale
+sed -i 's/AcceptEnv/#AcceptEnv/g' /etc/ssh/sshd_config
+
+# // install
+apt-get --reinstall --fix-missing install -y bzip2 gzip coreutils wget screen rsyslog iftop htop net-tools zip unzip wget net-tools curl nano sed screen gnupg gnupg1 bc apt-transport-https build-essential dirmngr libxml-parser-perl neofetch git lsof
+
+
+# install webserver
+apt -y install nginx php php-fpm php-cli php-mysql libxml-parser-perl
+rm /etc/nginx/sites-enabled/default
+rm /etc/nginx/sites-available/default
+curl ${REPO}ssh/nginx.conf > /etc/nginx/nginx.conf
+sed -i 's/listen = \/var\/run\/php-fpm.sock/listen = 127.0.0.1:9000/g' /etc/php/fpm/pool.d/www.conf
+mkdir -p /var/www/html
+echo "<?php phpinfo() ?>" > /var/www/html/info.php
+chown -R www-data:www-data /var/www/html
+chmod -R g+rw /var/www/html
+cd /var/www/html
+#wget -O /var/www/html/index.html "${REPO}ssh/index.html1"
+
+cat > /var/www/html/index.html <<-END
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<html>
+<kepala>
+<meta http-equiv="REFRESH" content="0;url=https://wa.me/6282131861788">
+</kepala>
+<tubuh>
+<p>Pengalihan URL</p>
+</tubuh>
+</html>
+END
+/etc/init.d/nginx restart
+
+# install badvpn
+cd
+wget -O /usr/sbin/badvpn "${REPO}ssh/badvpn" >/dev/null 2>&1
+chmod +x /usr/sbin/badvpn > /dev/null 2>&1
+wget -q -O /etc/systemd/system/badvpn1.service "${REPO}ssh/badvpn1.service" >/dev/null 2>&1
+wget -q -O /etc/systemd/system/badvpn2.service "${REPO}ssh/badvpn2.service" >/dev/null 2>&1
+wget -q -O /etc/systemd/system/badvpn3.service "${REPO}ssh/badvpn3.service" >/dev/null 2>&1
+systemctl disable badvpn1 
+systemctl stop badvpn1 
+systemctl enable badvpn1
+systemctl start badvpn1 
+systemctl disable badvpn2 
+systemctl stop badvpn2 
+systemctl enable badvpn2
+systemctl start badvpn2 
+systemctl disable badvpn3 
+systemctl stop badvpn3 
+systemctl enable badvpn3
+systemctl start badvpn3 
+
+
+# setting port ssh
+cd
+sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+sed -i '/Port 22/a Port 500' /etc/ssh/sshd_config
+sed -i '/Port 22/a Port 40000' /etc/ssh/sshd_config
+sed -i '/Port 22/a Port 51443' /etc/ssh/sshd_config
+sed -i '/Port 22/a Port 58080' /etc/ssh/sshd_config
+sed -i '/Port 22/a Port 200' /etc/ssh/sshd_config
+sed -i '/Port 22/a Port 22' /etc/ssh/sshd_config
+/etc/init.d/ssh restart
+
+echo "=== Install Dropbear ==="
+# install dropbear
+apt -y install dropbear
+sudo dropbearkey -t dss -f /etc/dropbear/dropbear_dss_host_key
+sudo chmod 600 /etc/dropbear/dropbear_dss_host_key
+wget -O /etc/default/dropbear "${REPO}ssh/dropbear"
+echo "/bin/false" >> /etc/shells
+echo "/usr/sbin/nologin" >> /etc/shells
+/etc/init.d/ssh restart
+/etc/init.d/dropbear restart
+#wget -q ${REPO}ssh/setrsyslog.sh && chmod +x setrsyslog.sh && ./setrsyslog.sh
+
+detect_os() {
+  if [[ -f /etc/os-release ]]; then
+    source /etc/os-release
+    echo "$ID $VERSION_ID"  # Mengembalikan ID dan versi OS
+  else
+    echo "Unknown"
+  fi
+}
+
+os_version=$(detect_os)
+if [[ "$os_version" =~ "ubuntu 24" ]]; then 
+  RSYSLOG_FILE="/etc/rsyslog.d/50-default.conf"
+elif [[ "$os_version" == "debian 12" ]]; then
+  RSYSLOG_FILE="/etc/rsyslog.conf"
+else
+  echo "Sistem operasi atau versi tidak dikenali. Keluar..."
+  exit 1
+fi
+
+LOG_FILES=(
+  "/var/log/auth.log"
+  "/var/log/kern.log"
+  "/var/log/mail.log"
+  "/var/log/user.log"
+  "/var/log/cron.log"
+)
+
+set_permissions() {
+  for log_file in "${LOG_FILES[@]}"; do
+    if [[ -f "$log_file" ]]; then
+      echo "Mengatur izin dan kepemilikan untuk $log_file..."
+      chmod 640 "$log_file"
+      chown syslog:adm "$log_file"  # Memberikan kepemilikan kepada syslog agar bisa menulis log
+    else
+      echo "$log_file tidak ditemukan, melewati..."
+    fi
+  done
+}
+
+# Mengecek apakah konfigurasi untuk dropbear sudah ada
+check_dropbear_log() {
+  grep -q 'if \$programname == "dropbear"' "$RSYSLOG_FILE"
+}
+
+# Fungsi untuk menambahkan konfigurasi dropbear
+add_dropbear_log() {
+  echo "Menambahkan konfigurasi Dropbear ke $RSYSLOG_FILE..."
+  sudo bash -c "echo -e 'if \$programname == \"dropbear\" then /var/log/auth.log\n& stop' >> $RSYSLOG_FILE"
+  systemctl restart rsyslog
+  echo "Konfigurasi Dropbear ditambahkan dan Rsyslog direstart."
+}
+
+if check_dropbear_log; then
+  echo "Konfigurasi Dropbear sudah ada, tidak ada perubahan yang dilakukan."
+else
+  add_dropbear_log
+fi
+
+# Set permissions untuk file log
+set_permissions
+
+if [[ "$OS_NAME" == "debian" && "$OS_VERSION" == "10" ]] || [[ "$OS_NAME" == "ubuntu" && "$OS_VERSION" == "20.04" ]]; then
+    echo "Menginstal squid3 untuk Debian 10 atau Ubuntu 20.04..."
+    apt -y install squid3
+else
+    echo "Menginstal squid untuk versi lain..."
+    apt -y install squid
+fi
+# Unduh file konfigurasi
+echo "Mengunduh file konfigurasi Squid..."
+wget -O /etc/squid/squid.conf "${REPO}ssh/squid3.conf"
+
+# Ganti placeholder dengan alamat IP
+echo "Mengganti placeholder IP dengan alamat IP saat ini..."
+sed -i $MYIP2 /etc/squid/squid.conf
+
+echo "Instalasi dan konfigurasi Squid selesai."
+# setting vnstat
+apt -y install vnstat
+/etc/init.d/vnstat restart
+apt -y install libsqlite3-dev
+wget https://humdi.net/vnstat/vnstat-2.6.tar.gz
+tar zxvf vnstat-2.6.tar.gz
+cd vnstat-2.6
+./configure --prefix=/usr --sysconfdir=/etc && make && make install
+cd
+vnstat -i $NET
+sed -i 's/Interface "'""eth0""'"/Interface "'""$NET""'"/g' /etc/vnstat.conf
+chown vnstat:vnstat /var/lib/vnstat -R
+systemctl enable vnstat
+/etc/init.d/vnstat restart
+rm -f /root/vnstat-2.6.tar.gz
+rm -rf /root/vnstat-2.6
+
+
+cd
+apt install haproxy -y
+wget -O /etc/haproxy/haproxy.cfg "https://raw.githubusercontent.com/kipasu/f/ssh/main/haproxy.cfg"
+systemctl daemon-reload
+systemctl stop haproxy
+systemctl enable haproxy
+systemctl start haproxy
+
+#OpenVPN
+wget ${REPO}ssh/vpn.sh &&  chmod +x vpn.sh && ./vpn.sh
+
+# // install lolcat
+clear
+# install Ruby & Yum
+apt-get install ruby -y
+# install lolcat
+wget https://github.com/busyloop/lolcat/archive/master.zip
+unzip master.zip
+rm -f master.zip
+cd lolcat-master/bin
+gem install lolcat
+# install figlet
+apt-get install figlet
+# Install figlet ascii
+sudo apt-get install figlet
+git clone https://github.com/busyloop/lolcat
+cd lolcat/bin && gem install lolcat
+cd /usr/share
+git clone https://github.com/xero/figlet-fonts
+mv figlet-fonts/* figlet && rm –rf figlet-fonts
+rm -r /root/*
+cd
+
+# memory swap 2gb
+cd
+dd if=/dev/zero of=/swapfile bs=2048 count=1048576
+mkswap /swapfile
+chown root:root /swapfile
+chmod 0600 /swapfile >/dev/null 2>&1
+swapon /swapfile >/dev/null 2>&1
+sed -i '$ i\/swapfile      swap swap   defaults    0 0' /etc/fstab
+
+# install fail2ban
+apt -y install fail2ban
+
+# Instal DDOS Flate
+if [ -d '/usr/local/ddos' ]; then
+	echo; echo; echo "Please un-install the previous version first"
+	exit 0
+else
+	mkdir /usr/local/ddos
+fi
+clear
+echo; echo 'Installing DOS-Deflate 0.6'; echo
+echo; echo -n 'Downloading source files...'
+wget -q -O /usr/local/ddos/ddos.conf http://www.inetbase.com/scripts/ddos/ddos.conf
+echo -n '.'
+wget -q -O /usr/local/ddos/LICENSE http://www.inetbase.com/scripts/ddos/LICENSE
+echo -n '.'
+wget -q -O /usr/local/ddos/ignore.ip.list http://www.inetbase.com/scripts/ddos/ignore.ip.list
+echo -n '.'
+wget -q -O /usr/local/ddos/ddos.sh http://www.inetbase.com/scripts/ddos/ddos.sh
+chmod 0755 /usr/local/ddos/ddos.sh
+cp -s /usr/local/ddos/ddos.sh /usr/local/bin/ddos
+echo '...done'
+echo; echo -n 'Creating cron to run script every minute.....(Default setting)'
+/usr/local/ddos/ddos.sh --cron > /dev/null 2>&1
+echo '.....done'
+echo; echo 'Installation has completed.'
+echo 'Config file is at /usr/local/ddos/ddos.conf'
+echo 'Please send in your comments and/or suggestions to https://t.me/newbie_store24'
+
+# banner /etc/issue.net
+echo "Banner /etc/issue.net" >>/etc/ssh/sshd_config
+
+# Ganti Banner
+wget -O /etc/issue.net "${REPO}ssh/issue.net"
+
+#install bbr dan optimasi kernel
+wget ${REPO}ssh/bbr.sh && chmod +x bbr.sh && ./bbr.sh
+
+wget -q ${REPO}ssh/ipserver && chmod +x ipserver && ./ipserver
+# blokir torrent
+iptables -A FORWARD -m string --string "get_peers" --algo bm -j DROP
+iptables -A FORWARD -m string --string "announce_peer" --algo bm -j DROP
+iptables -A FORWARD -m string --string "find_node" --algo bm -j DROP
+iptables -A FORWARD -m string --algo bm --string "BitTorrent" -j DROP
+iptables -A FORWARD -m string --algo bm --string "BitTorrent protocol" -j DROP
+iptables -A FORWARD -m string --algo bm --string "peer_id=" -j DROP
+iptables -A FORWARD -m string --algo bm --string ".torrent" -j DROP
+iptables -A FORWARD -m string --algo bm --string "announce.php?passkey=" -j DROP
+iptables -A FORWARD -m string --algo bm --string "torrent" -j DROP
+iptables -A FORWARD -m string --algo bm --string "announce" -j DROP
+iptables -A FORWARD -m string --algo bm --string "info_hash" -j DROP
+iptables-save > /etc/iptables.up.rules
+iptables-restore -t < /etc/iptables.up.rules
+netfilter-persistent save
+netfilter-persistent reload
+rm ipserver
+
+# download script
+wget -O /etc/issue.net "${REPO}ssh/issue.net"
+cd
+
+cat> /etc/cron.d/auto_exp << END
+SHELL=/bin/sh
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+0 0 * * * root /usr/local/sbin/xp
+END
+
+cat> /etc/cron.d/daily_backup << END
+SHELL=/bin/sh
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+0 22 * * * root /usr/local/sbin/backup
+END
+
+cat >/etc/cron.d/logclean <<-END
+SHELL=/bin/sh
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+*/10 * * * * root truncate -s 0 /var/log/syslog \
+    && truncate -s 0 /var/log/nginx/error.log \
+    && truncate -s 0 /var/log/nginx/access.log \
+    && truncate -s 0 /var/log/xray/error.log \
+    && truncate -s 0 /var/log/xray/access.log
+END
+
+cat >/etc/cron.d/daily_reboot <<-END
+SHELL=/bin/sh
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+5 0 * * * root /sbin/reboot
+END
+
+service cron restart >/dev/null 2>&1
+service cron reload >/dev/null 2>&1
+service cron start >/dev/null 2>&1
+
+# remove unnecessary files
+apt autoclean -y >/dev/null 2>&1
+apt -y remove --purge unscd >/dev/null 2>&1
+apt-get -y --purge remove samba* >/dev/null 2>&1
+apt-get -y --purge remove apache2* >/dev/null 2>&1
+apt-get -y --purge remove bind9* >/dev/null 2>&1
+apt-get -y remove sendmail* >/dev/null 2>&1
+apt autoremove -y >/dev/null 2>&1
+# finishing
+cd
+chown -R www-data:www-data /var/www/html
+
+rm -f /root/key.pem
+rm -f /root/cert.pem
+rm -f /root/bbr.sh
+rm -rf /etc/apache2
+
+clear
