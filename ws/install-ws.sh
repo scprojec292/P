9560ec100483101d2f8939e1b9199d71373493f6
Andyyuda <128485068+Andyyuda@users.noreply.github.com>
2025-02-09 17:07:02 +0700

    Update install-ws.sh

--- ws/install-ws.sh
+++ ws/install-ws.sh
@@ -1,44 +1,21 @@
 #!/bin/bash
 
-rm -f $0
-
-file_path="/etc/handeling"
-repo="https://raw.githubusercontent.com/Andyyuda/P/main"
-
 apt update
 apt install python3 -y
 apt install python3-pip -y
 apt install python3-requests -y
 
-# Cek apakah file ada
-if [ ! -f "$file_path" ]; then
-    # Jika file tidak ada, buat file dan isi dengan dua baris
-    echo -e "Switching Protocols\nYellow" | sudo tee "$file_path" > /dev/null
-    echo "File '$file_path' berhasil dibuat."
-else
-    # Jika file ada, cek apakah isinya kosong
-    if [ ! -s "$file_path" ]; then
-        # Jika file ada tetapi kosong, isi dengan dua baris
-        echo -e "Switching Protocols\nYellow" | sudo tee "$file_path" > /dev/null
-        echo "File '$file_path' kosong dan telah diisi."
-    else
-        # Jika file ada dan berisi data, tidak lakukan apapun
-        echo "File '$file_path' sudah ada dan berisi data."
-    fi
-fi
+mkdir -p /etc/websocket
 
-cd /usr/local/bin
-wget -q -O vpn.zip "${repo}/ws/vpn.zip"
-unzip vpn.zip
-cp ws ws-ovpn
-chmod +x ws ws-ovpn
-rm vpn.zip
-cd
+repo="https://raw.githubusercontent.com/Andyyuda/P/main"
+
+wget -q -O /etc/websocket/ws.py "${repo}/ws/ws.py"
+chmod +x /etc/websocket/ws.py
 
 # Installing Service
 cat > /etc/systemd/system/ws.service << END
 [Unit]
-Description=Proxy Mod
+Description=Websocket
 Documentation=https://google.com
 After=network.target nss-lookup.target
 
@@ -48,7 +25,7 @@ User=root
 CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
 AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
 NoNewPrivileges=true
-ExecStart=/usr/local/bin/ws
+ExecStart=/usr/bin/python3 -O /etc/websocket/ws.py 10015
 Restart=on-failure
 
 [Install]
@@ -63,7 +40,7 @@ systemctl restart ws.service
 # Installing Service
 cat > /etc/systemd/system/ws-ovpn.service << END
 [Unit]
-Description=Proxy Mod
+Description=OpenVPN
 Documentation=https://google.com
 After=network.target nss-lookup.target
 
@@ -73,7 +50,7 @@ User=root
 CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
 AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
 NoNewPrivileges=true
-ExecStart=/usr/local/bin/ws-ovpn 2086
+ExecStart=/usr/bin/python3 -O /etc/websocket/ws.py 10012
 Restart=on-failure
 
 [Install]
@@ -84,3 +61,5 @@ systemctl daemon-reload
 systemctl enable ws-ovpn
 systemctl start ws-ovpn
 systemctl restart ws-ovpn
+
+rm -f $0
