0a1d40cd1c5fb3d7b05d56c70ab36ec64a1e4694
scprojec292 <scprojec292@gmail.com>
2025-08-12 10:52:49 +0700

    Update update.sh

--- menu/update.sh
+++ menu/update.sh
@@ -5,12 +5,12 @@ apt update -y
 apt install -y unzip wget
 
 # Mengunduh dan mengekstrak menu.zip
-wget -q https://raw.githubusercontent.com/Andyyuda/v1/main/menu/menu.zip -O menu.zip
+wget -q https://raw.githubusercontent.com/scprojec292/P/main/menu/menu.zip -O menu.zip
 unzip menu.zip
 chmod +x menu/*
 mv menu/* /usr/local/sbin
 rm -rf menu menu.zip
 
 # Mengunduh versionbaru dan menggantinya menjadi "version"
-wget -q https://raw.githubusercontent.com/Andyyuda/P/main/versionbaru -O /usr/local/sbin/version
+wget -q https://raw.githubusercontent.com/scprojec292/P/main/versionbaru -O /usr/local/sbin/version
 chmod +x /usr/local/sbin/version
